c412fddecf6028c2f066aac723a265278f825c92
John Graham <johnegraham2@gmail.com>
2024-12-28 10:15:10 -0500

    rotation, prev, next

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ class HaloSlider {
   slides: HTMLElement[] = [];
   offset: number = 0;
   currentRotation: number = 0;
+  currentSlideIndex: number = 0;
 
   constructor(selector: string) {
     this.sliderEl = document.querySelector(selector);
@@ -15,23 +16,27 @@ class HaloSlider {
     }
 
     this.radius = this.sliderEl ? this.sliderEl.clientWidth / 2 : 0;
-    this.slides = [...document.querySelectorAll<HTMLElement>(".halo-slide")];
+    this.slides = [
+      ...this.sliderEl.querySelectorAll<HTMLElement>(".halo-slide"),
+    ];
 
     this.createSlides();
+
+    const nextBtn = this.sliderEl.querySelector(".halo-slider-next");
+    const prevBtn = this.sliderEl.querySelector(".halo-slider-prev");
+
+    nextBtn?.addEventListener("click", this.onClickNext.bind(this));
+    prevBtn?.addEventListener("click", this.onClickPrev.bind(this));
   }
 
   createSlides() {
-    const slideEls = document.querySelectorAll<HTMLElement>(".halo-slide");
-    this.offset = this.getOffsetDegrees(slideEls.length);
+    this.offset = this.getOffsetDegrees(this.slides.length);
 
-    slideEls.forEach((slideEl, index) => {
+    this.slides.forEach((slideEl, index) => {
       const [xpos, ypos] = this.getCirclePosition(index);
       slideEl.style.setProperty("--xpos", `${xpos}px`);
       slideEl.style.setProperty("--ypos", `${ypos}px`);
-      slideEl.addEventListener("click", (e) => {
-        this.onClickSlide(e);
-      });
-      this.slides.push(slideEl);
+      slideEl.addEventListener("click", this.onClickSlide.bind(this));
     });
   }
 
@@ -51,18 +56,40 @@ class HaloSlider {
     return this.slides.indexOf(slideEl);
   }
 
-  onClickSlide(e: MouseEvent) {
+  onClickSlide(e: Event) {
     const slideEl = e?.target as HTMLElement;
     if (!slideEl) return;
 
-    const index = this.getSlideIndex(slideEl);
-    const degree = index * this.offset;
+    const newIndex = this.getSlideIndex(slideEl);
 
-    this.rotateSlider(degree);
+    this.rotateSlider(newIndex);
   }
 
-  rotateSlider(degree: number) {
-    this.currentRotation = degree;
+  onClickNext(e: Event) {
+    e?.preventDefault();
+    let index = this.currentSlideIndex + 1;
+    index = index >= this.slides.length ? 0 : index;
+    this.rotateSlider(index);
+  }
+
+  onClickPrev(e: Event) {
+    e?.preventDefault();
+    let index = this.currentSlideIndex - 1;
+    index = index < 0 ? this.slides.length - 1 : index;
+    this.rotateSlider(index);
+  }
+
+  rotateSlider(newIndex: number) {
+    let indexOffset = newIndex - this.currentSlideIndex;
+    indexOffset =
+      indexOffset < 0 ? indexOffset + this.slides.length : indexOffset;
+
+    this.currentSlideIndex = newIndex;
+
+    let degree = indexOffset * this.offset;
+    degree = degree > 180 ? degree - 360 : degree;
+
+    this.currentRotation = this.currentRotation + degree;
 
     this.sliderEl?.style.setProperty(
       "--rotation",
